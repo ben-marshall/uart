325367caf1fe008198c17ce15007ec108a14f65f
Ben Marshall <bm12656@my.bristol.ac.uk>
2017-01-27 16:10:19 +0000

    Fix source path

--- bin/source.me.sh
+++ bin/source.me.sh
@@ -3,6 +3,6 @@ XILINX_PATH=/opt/Xilinx
 VIVADO_PATH=$XILINX_PATH/Vivado/2016.4
 
 echo "Vivado Path: $VIVADO_PATH"
-source $VIVADO_PATH/settings.sh
+source $VIVADO_PATH/settings64.sh
 
 echo "Run 'vivado' to start the Vivado suite."
